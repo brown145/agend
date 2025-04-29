839bff1ad08126e09737ec46e63aef71402a5f83
Scott Brown <brown145@gmail.com>
2025-04-28 21:20:27 -0700

    convex todolist

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+"use client";
+
+import { useQuery } from "convex/react";
+import { api } from "../../convex/_generated/api";
+
 export default function Home() {
+  const tasks = useQuery(api.tasks.get);
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <main>{tasks?.map(({ _id, text }) => <div key={_id}>{text}</div>)}</main>
   );
 }
