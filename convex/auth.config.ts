83c82fb486e4ce9b006d95109047df0d055695f6
Scott Brown <brown145@gmail.com>
2025-05-12 22:55:12 -0700

    rename key

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,7 +1,7 @@
 export default {
   providers: [
     {
-      domain: "https://relaxing-bunny-64.clerk.accounts.dev",
+      domain: process.env.CLERK_JWT_ISSUER_DOMAIN,
       applicationID: "convex",
     },
   ],
