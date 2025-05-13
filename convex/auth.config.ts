65e32d1417ad0e6fe89818f6cce4e3af9910f12f
Scott Brown <brown145@gmail.com>
2025-05-12 23:30:58 -0700

    dev?

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,8 +1,9 @@
 export default {
   providers: [
     {
-      // domain: process.env.NEXT_PUBLIC_CLERK_JWT_ISSUER_DOMAIN,
-      domain: "https://clerk.agendry.com/.well-known/jwks.json",
+      // TODO: figurie out env issues later
+      // domain: process.env.NEXT_PUBLIC_CLERK_FRONTEND_API_URL,
+      domain: "https://relaxing-bunny-64.clerk.accounts.dev",
       applicationID: "convex",
     },
   ],
