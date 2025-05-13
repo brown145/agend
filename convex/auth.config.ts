b1036cb2b59d6ebc4f784156aeb81b8f1a7e20b5
Scott Brown <brown145@gmail.com>
2025-05-12 22:42:07 -0700

    prod auth confit?

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -2,8 +2,8 @@ export default {
   providers: [
     {
       // TODO: figurie out env issues later
-      // domain: process.env.NEXT_PUBLIC_CLERK_FRONTEND_API_URL,
-      domain: "https://relaxing-bunny-64.clerk.accounts.dev",
+      domain: process.env.NEXT_PUBLIC_CLERK_FRONTEND_API_URL,
+      // domain: "https://relaxing-bunny-64.clerk.accounts.dev",
       applicationID: "convex",
     },
   ],
