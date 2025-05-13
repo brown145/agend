1ec830a3050bc185bbeb9e931432d1496a96565f
Scott Brown <brown145@gmail.com>
2025-05-12 22:48:15 -0700

    root layout update

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,9 +1,7 @@
 export default {
   providers: [
     {
-      // TODO: figurie out env issues later
-      domain: process.env.NEXT_PUBLIC_CLERK_FRONTEND_API_URL,
-      // domain: "https://relaxing-bunny-64.clerk.accounts.dev",
+      domain: "https://relaxing-bunny-64.clerk.accounts.dev",
       applicationID: "convex",
     },
   ],
