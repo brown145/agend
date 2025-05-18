d7d54b16b9d42f93fdba66ae16d896a3129af610
Scott Brown <brown145@gmail.com>
2025-05-18 11:12:21 -0700

    rename env var

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -4,7 +4,7 @@ export default {
   providers: [
     {
       domain:
-        process.env.NEXT_PUBLIC_CLERK_JWT_ISSUER_DOMAIN ??
+        process.env.CLERK_JWT_ISSUER_DOMAIN ??
         "https://bold-rodent-16.clerk.accounts.dev",
       applicationID: "convex",
     },
