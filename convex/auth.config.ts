2f6098629e70e514c678a46750aecf5ae3328322
Scott Brown <brown145@gmail.com>
2025-05-18 20:48:20 -0700

    env for issuer

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,10 +1,7 @@
-// This file is used for local development only
-// For production, use the deployment configuration in convex.json
 export default {
   providers: [
     {
-      domain: "https://clerk.agendry.com",
-      // domain: "https://bold-rodent-16.clerk.accounts.dev",
+      domain: process.env.CLERK_JWT_ISSUER_DOMAIN,
       applicationID: "convex",
     },
   ],
